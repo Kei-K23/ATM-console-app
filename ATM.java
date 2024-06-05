429d7316d2a5610775a506967f915584fb3335ff
Kei-K23 <keiksl2301@gmail.com>
2024-06-06 06:14:39 +0630

    Add transfter method to Bank class

--- ATM.java
+++ ATM.java
@@ -10,7 +10,20 @@ public class ATM {
     }
 
     public void run() {
-        System.out.println("Enter you ATM Account number");
+        System.out.println("  _____ ___  __  __ ");
+        System.out.println(" |_   _| __||  \\/  |");
+        System.out.println("   | | | _| | |\\/| |");
+        System.out.println("   |_| |___||_|  |_|");
+        System.out.println("                    ");
+        System.out.println("  __  __   ___   _  __ ");
+        System.out.println(" |  \\/  | |_ _| | |/ / ");
+        System.out.println(" | |\\/| |  | |  | ' /  ");
+        System.out.println(" |_|  |_| |___| |_|\\_\\ ");
+        System.out.println("                     ");
+        System.out.println("Welcome to the ATM console!");
+        System.out.println("##########################");
+
+        System.out.print("Enter you ATM Account number: ");
         final int ACCOUNT_NUMBER = scanner.nextInt();
 
         Account account = bank.findUserByAccountNumber(ACCOUNT_NUMBER);
@@ -32,35 +45,54 @@ public class ATM {
             System.out.println("1. Check Balance");
             System.out.println("2. Deposit");
             System.out.println("3. Withdraw");
-            System.out.println("4. Exit");
+            System.out.println("4. Transfer");
+            System.out.println("0. Exit");
             System.out.print("Choose an option: ");
 
             int choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
+                    System.out.println(" ");
                     double totalBalance = bank.checkBalance(ACCOUNT_NUMBER);
                     System.out.println("Total Balance :: " + totalBalance);
                     break;
                 case 2:
+                    System.out.println(" ");
+
                     System.out.println("Deposit");
                     System.out.println("How much amount to deposit");
                     double amountForDeposit = scanner.nextDouble();
-                    double newBalanceForDeposit = bank.deposit(ACCOUNT_NUMBER, amountForDeposit);
-                    System.out.println("New Balance :: " + newBalanceForDeposit);
+                    bank.deposit(ACCOUNT_NUMBER, amountForDeposit);
                     break;
                 case 3:
+                    System.out.println(" ");
+
                     System.out.println("Withdraw");
                     System.out.println("How much amount to withdraw");
                     double amountForWithdraw = scanner.nextDouble();
-                    double newBalanceForWithdraw = bank.withdraw(ACCOUNT_NUMBER, amountForWithdraw);
-                    System.out.println("New Balance :: " + newBalanceForWithdraw);
+                    bank.withdraw(ACCOUNT_NUMBER, amountForWithdraw);
                     break;
                 case 4:
+                    System.out.println(" ");
+
+                    System.out.println("Transfer");
+                    System.out.println("Enter account number to transfer");
+                    final int TO_ACCOUNT = scanner.nextInt();
+                    System.out.println("How much amount to transfer to " + TO_ACCOUNT);
+                    double transferAmount = scanner.nextDouble();
+
+                    bank.transfer(ACCOUNT_NUMBER, TO_ACCOUNT, transferAmount);
+                    break;
+                case 0:
+                    System.out.println(" ");
+
                     System.out.println("Exit");
                     System.out.println("Thank you for using ATM console!");
                     return;
                 default:
+                    System.out.println(" ");
+
                     System.out.println("Invalid option! Please try again.");
             }
         }
--- Bank.java
+++ Bank.java
@@ -2,8 +2,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Bank {
     public Account findUserByAccountNumber(int accountNumber) {
@@ -39,129 +37,127 @@ public class Bank {
         return null;
     }
 
-   public double checkBalance(int accountNumber) {
-       try {
-           DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-           Connection connection = databaseConnection.getConnection();
-           Account account = new Account();
-
-           // Example query
-           String query = "SELECT total_balance FROM accounts WHERE account_number = ?";
-           PreparedStatement preparedStatement = connection.prepareStatement(query);
-           preparedStatement.setInt(1, accountNumber);  // Example account number
-
-           ResultSet resultSet = preparedStatement.executeQuery();
-           while (resultSet.next()) {
-               account.setTotalBalance(resultSet.getDouble("total_balance"));
-           }
-
-           resultSet.close();
-           preparedStatement.close();
-
-           return account.getTotalBalance();
-       } catch (SQLException e) {
-           // TODO handle exception
-           e.printStackTrace();
-       }
-
-       // never hit this
-       return 0.00;
-   }
-
-   public double deposit(int accountNumber, double amount) {
-       if (amount < 0) {
-           throw new IllegalArgumentException("insufficient amount");
-       }
+    public double checkBalance(int accountNumber) {
         try {
-           double totalBalance = checkBalance(accountNumber);
+            DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+            Connection connection = databaseConnection.getConnection();
+            Account account = new Account();
 
-           DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-           Connection connection = databaseConnection.getConnection();
+            // Example query
+            String query = "SELECT total_balance FROM accounts WHERE account_number = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, accountNumber);  // Example account number
 
-           // Example query
-           String query = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
-           PreparedStatement updateStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                account.setTotalBalance(resultSet.getDouble("total_balance"));
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+            return account.getTotalBalance();
+        } catch (SQLException e) {
+            // TODO handle exception
+            e.printStackTrace();
+        }
+
+        // never hit this
+        return 0.00;
+    }
+
+    public void deposit(int accountNumber, double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("insufficient amount");
+        }
+        try {
+            double totalBalance = checkBalance(accountNumber);
+
+            DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+            Connection connection = databaseConnection.getConnection();
+
+            // Example query
+            String query = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
+            PreparedStatement updateStatement = connection.prepareStatement(query);
             updateStatement.setDouble(1, totalBalance + amount);
             updateStatement.setInt(2, accountNumber);
 
-           updateStatement.executeUpdate();
-           updateStatement.close();
+            updateStatement.executeUpdate();
+            updateStatement.close();
+            System.out.println("New Balance :: " + (totalBalance + amount));
+        } catch (SQLException e) {
+            // TODO handle exception
+            e.printStackTrace();
+        }
+    }
 
-            return checkBalance(accountNumber);
-
-       } catch (SQLException e) {
-           // TODO handle exception
-           e.printStackTrace();
-       }
-        // never hit this
-        return 0.00;
-   }
-
-   public double withdraw(int accountNumber, double amount) {
-       if (amount < 0) {
-           throw new IllegalArgumentException("insufficient amount");
-       }
+    public void withdraw(int accountNumber, double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("insufficient amount");
+        }
         try {
-           double totalBalance = checkBalance(accountNumber);
+            double totalBalance = checkBalance(accountNumber);
 
-           if (amount > totalBalance) {
-               throw new IllegalArgumentException("insufficient amount");
-           }
+            if (amount > totalBalance) {
+                throw new IllegalArgumentException("insufficient amount");
+            }
 
-           DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-           Connection connection = databaseConnection.getConnection();
+            DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+            Connection connection = databaseConnection.getConnection();
 
-           // Example query
-           String query = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
-           PreparedStatement updateStatement = connection.prepareStatement(query);
+            // Example query
+            String query = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
+            PreparedStatement updateStatement = connection.prepareStatement(query);
             updateStatement.setDouble(1, totalBalance - amount);
             updateStatement.setInt(2, accountNumber);
 
-           updateStatement.executeUpdate();
-           updateStatement.close();
+            updateStatement.executeUpdate();
+            updateStatement.close();
 
-            return checkBalance(accountNumber);
+            System.out.println("New Balance :: " + (totalBalance - amount));
+        } catch (SQLException e) {
+            // TODO handle exception
+            e.printStackTrace();
+        }
+    }
 
-       } catch (SQLException e) {
-           // TODO handle exception
-           e.printStackTrace();
-       }
-
-       // never hit this
-       return 0.00;
-   }
-
-   public double withdraw(int accountNumber, double amount) {
-       if (amount < 0) {
-           throw new IllegalArgumentException("insufficient amount");
-       }
+    public void transfer(int fromAccountNumber, int toAccountNumber, double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("insufficient amount");
+        }
         try {
-           double totalBalance = checkBalance(accountNumber);
+            double totalBalanceForFromAccount = checkBalance(fromAccountNumber);
+            double totalBalanceForToAccount = checkBalance(toAccountNumber);
 
-           if (amount > totalBalance) {
-               throw new IllegalArgumentException("insufficient amount");
-           }
+            if (amount > totalBalanceForFromAccount) {
+                throw new IllegalArgumentException("insufficient amount");
+            }
 
-           DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-           Connection connection = databaseConnection.getConnection();
+            DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+            Connection connection = databaseConnection.getConnection();
 
-           // Example query
-           String query = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
-           PreparedStatement updateStatement = connection.prepareStatement(query);
-            updateStatement.setDouble(1, totalBalance - amount);
-            updateStatement.setInt(2, accountNumber);
+            // Example query
+            String updateQueryForToAccountNumber = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
+            PreparedStatement updateStatementForToAccountNumber = connection.prepareStatement(updateQueryForToAccountNumber);
+            updateStatementForToAccountNumber.setDouble(1, totalBalanceForToAccount + amount);
+            updateStatementForToAccountNumber.setInt(2, toAccountNumber);
 
-           updateStatement.executeUpdate();
-           updateStatement.close();
+            updateStatementForToAccountNumber.executeUpdate();
+            updateStatementForToAccountNumber.close();// Example query
 
-            return checkBalance(accountNumber);
+            String updateQueryForFromAccountNumber = "UPDATE accounts SET total_balance = ? WHERE account_number = ?";
+            PreparedStatement updateStatementForFromAccountNumber = connection.prepareStatement(updateQueryForFromAccountNumber);
+            updateStatementForFromAccountNumber.setDouble(1, totalBalanceForFromAccount - amount);
+            updateStatementForFromAccountNumber.setInt(2, fromAccountNumber);
 
-       } catch (SQLException e) {
-           // TODO handle exception
-           e.printStackTrace();
-       }
+            updateStatementForFromAccountNumber.executeUpdate();
+            updateStatementForFromAccountNumber.close();
 
-       // never hit this
-       return 0.00;
-   }
+            System.out.println(fromAccountNumber + " account have :: " + (totalBalanceForFromAccount - amount) + " total balance after transfer to " + toAccountNumber);
+            System.out.println(toAccountNumber + " account have :: " + (totalBalanceForToAccount + amount) + " total balance after received from " + fromAccountNumber);
+        } catch (SQLException e) {
+            // TODO handle exception
+            e.printStackTrace();
+        }
+    }
 }
