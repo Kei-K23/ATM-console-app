0c350378e7daffc801ebf7d6f37b7ff6d684e369
Kei-K23 <keiksl2301@gmail.com>
2024-06-05 18:43:39 +0630

    Refactor the code and organize the class

--- ATM.java
+++ ATM.java
@@ -1,3 +1,60 @@
-public class ATM {
+import java.util.Scanner;
 
+public class ATM {
+    private Bank bank;
+    private Scanner scanner;
+
+    public ATM(Bank bank) {
+        this.bank = bank;
+        scanner = new Scanner(System.in);
+    }
+
+    public void run() {
+        System.out.println("Enter you ATM Account number");
+        final String ACCOUNT_NUMBER = scanner.nextLine();
+
+        Account account = bank.findUserByAccountNumber(ACCOUNT_NUMBER);
+
+        if (account == null) {
+            System.out.println("Invalid Account Number");
+            return;
+        }
+
+        System.out.print("Enter your PIN: ");
+        final int PIN = scanner.nextInt();
+
+        if (!account.isValidate(PIN)) {
+            System.out.println("Invalid Account PIN");
+            return;
+        }
+
+        while (true) {
+            System.out.println("\nATM Menu:");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Check Balance");
+                    break;
+                case 2:
+                    System.out.println("Deposit");
+                    break;
+                case 3:
+                    System.out.println("Withdraw");
+                    break;
+                case 4:
+                    System.out.println("Exit");
+                    System.out.println("Thank you for using ATM console!");
+                    return;
+                default:
+                    System.out.println("Invalid option! Please try again.");
+            }
+        }
+    }
 }
--- ATMConsoleApp.java
+++ ATMConsoleApp.java
@@ -1,23 +1,9 @@
-import java.util.Scanner;
-
 public class ATMConsoleApp {
     public static void main(String[] args) {
-        // Entry point of the program
-        // Create a Scanner object
-        Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank();
+        ATM atm = new ATM(bank);
 
-        System.out.println("Enter you ATM Account number");
-        final int ACCOUNT_NUMBER = scanner.nextInt();
-
-        System.out.println("Enter you ATM PIN");
-        final int PIN = scanner.nextInt();
-
-        System.out.println("PIN : " + ACCOUNT_NUMBER + " AND PASSWORD " + PIN);
-
-        scanner.close();
-
-        Database database = new Database();
-        Account account = database.findUserByAccountNumber(ACCOUNT_NUMBER);
-        System.out.println(account.getAccountNumber() + account.getUsername());
+        // run atm application
+        atm.run();
     }
 }
