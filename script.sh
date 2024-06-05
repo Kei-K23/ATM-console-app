b4d5c41749b4ed5c51f543f15c35de952a51a37a
Kei-K23 <keiksl2301@gmail.com>
2024-06-05 19:00:03 +0630

    Add bash script to perform compile, run and clean

---
+++ script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Define the paths and filenames
+JAVAC=javac
+JAVA=java
+CLASSPATH=.:lib/mysql-connector-j-8.4.0.jar
+SRC=ATMConsoleApp.java
+MAIN=ATMConsoleApp
+
+# Function to compile the Java source file
+compile() {
+    echo "Compiling $SRC..."
+    $JAVAC -cp $CLASSPATH $SRC
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed."
+        exit 1
+    fi
+    echo "Compilation successful."
+}
+
+# Function to run the compiled Java application
+run() {
+    compile
+    echo "Running $MAIN..."
+    $JAVA -cp $CLASSPATH $MAIN
+}
+
+# Function to clean up generated class files
+clean() {
+    echo "Cleaning up..."
+    rm -f *.class
+    echo "Clean up successful."
+}
+
+# Parse command line arguments to determine which function to execute
+case $1 in
+    compile)
+        compile
+        ;;
+    run)
+        run
+        ;;
+    clean)
+        clean
+        ;;
+    *)
+        echo "Usage: $0 {compile|run|clean}"
+        exit 1
+        ;;
+esac
